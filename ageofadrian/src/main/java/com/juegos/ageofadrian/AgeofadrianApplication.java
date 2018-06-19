510223e0723f7b7f60ab38ac51a81dfe08563cd0
Daniel Lopera <dlopera@tecnosoftware.com>
2018-06-19 15:36:31 -0300

    Iniciando proyecto

---
+++ ageofadrian/src/main/java/com/juegos/ageofadrian/AgeofadrianApplication.java
@@ -0,0 +1,12 @@
+package com.juegos.ageofadrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeofadrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeofadrianApplication.class, args);
+	}
+}
